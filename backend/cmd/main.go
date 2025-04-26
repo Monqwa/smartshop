faffefeb0b0566b0b8e1c5b08ec3f3516fb01ab8
Monqwa <edumonnqwwa@gmail.com>
2025-04-27 01:29:45 +0500

    FIX CORS

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -37,6 +37,18 @@ func RunHttpServer() {
 		})
 	})
 
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization")
+
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		c.Next()
+	})
+
 	handlers.NewItemRouter(r)
 	handlers.NewListRouter(r)
 
